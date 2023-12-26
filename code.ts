04d65faef84566b14735e245b9b82ba11fb4b72f
abhishekshankr <abhishekshankar241@gmail.com>
2023-12-25 23:22:45 -0500

    Fixed alphabetical sort order for icon display

--- code.ts
+++ code.ts
@@ -3,8 +3,12 @@ figma.showUI(__html__, { width: 390, height: 500 });
 figma.ui.onmessage = async msg => {
   if (msg.type === 'create-icon') {
     const svg = msg.svg;
+    const name = msg.name; // Use the name passed in the message
     const node = await figma.createNodeFromSvg(svg);
     
+    // Set the name of the node to the name passed in the message
+    node.name = name;
+
     // Set the position of the new node to the center of the viewport
     node.x = figma.viewport.center.x;
     node.y = figma.viewport.center.y;
