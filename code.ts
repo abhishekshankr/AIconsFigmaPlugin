c9f585c803ca0d3129cd78a0a0db3d2edd3d8f67
abhishekshankr <abhishekshankar241@gmail.com>
2023-12-31 02:01:08 -0500

    Icons are placed on integer co-ordinates

--- code.ts
+++ code.ts
@@ -5,13 +5,13 @@ figma.ui.onmessage = async msg => {
     const svg = msg.svg;
     const name = msg.name; // Use the name passed in the message
     const node = await figma.createNodeFromSvg(svg);
-    
+
     // Set the name of the node to the name passed in the message
     node.name = name;
 
     // Set the position of the new node to the center of the viewport
-    node.x = figma.viewport.center.x;
-    node.y = figma.viewport.center.y;
+    node.x = Math.round(figma.viewport.center.x);
+    node.y = Math.round(figma.viewport.center.y);
 
     node.constrainProportions = true;
 
@@ -19,8 +19,17 @@ figma.ui.onmessage = async msg => {
       node.resize(64, 64);
     }
 
-    // Add the new node to the current page
-    figma.currentPage.appendChild(node);
+    // Check if there is a frame or node selected
+    const selection = figma.currentPage.selection;
+    if (selection.length > 0 && selection[0].type === 'FRAME') {
+      // Append the node to the selected frame
+      selection[0].appendChild(node);
+      node.x = 0;
+      node.y = 0;
+    } else {
+      // If no frame is selected, add the new node to the current page
+      figma.currentPage.appendChild(node);
+    }
 
     // Select the new node
     figma.currentPage.selection = [node];
