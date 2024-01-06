ead3fc3de96c8465943077efa183fead2db45635
abhishekshankr <abhishekshankar241@gmail.com>
2024-01-06 12:17:21 -0500

    Fixing icon display when inserted into a page

--- code.ts
+++ code.ts
@@ -1,37 +1,59 @@
 figma.showUI(__html__, { themeColors: true, width: 435, height: 500 });
 
+let lastAppendedIcon: SceneNode | null = null; // Track the last appended icon on the page
+
 figma.ui.onmessage = async msg => {
   if (msg.type === 'create-icon') {
     const svg = msg.svg;
-    const name = msg.name; // Use the name passed in the message
+    const name = msg.name;
     const node = await figma.createNodeFromSvg(svg);
-
-    // Set the name of the node to the name passed in the message
     node.name = name;
-
-    // Set the position of the new node to the center of the viewport
-    node.x = Math.round(figma.viewport.center.x);
-    node.y = Math.round(figma.viewport.center.y);
-
     node.constrainProportions = true;
 
     if (figma.editorType === 'figjam') {
       node.resize(64, 64);
     }
 
-    // Check if there is a frame or node selected
     const selection = figma.currentPage.selection;
-    if (selection.length > 0 && selection[0].type === 'FRAME') {
-      // Append the node to the selected frame
-      selection[0].appendChild(node);
-      node.x = 0;
-      node.y = 0;
+
+    if (selection.find(node => node === lastAppendedIcon) && lastAppendedIcon) {
+      appendToParentOrPage(node, lastAppendedIcon);
+    } else if (selection.length > 0) {
+      const selectedNode = selection[selection.length - 1];
+      appendToParentOrPage(node, selectedNode);
     } else {
-      // If no frame is selected, add the new node to the current page
-      figma.currentPage.appendChild(node);
+      appendToPage(node);
     }
 
-    // Select the new node
     figma.currentPage.selection = [node];
   }
 };
+
+function appendToPage(node: SceneNode) {
+  figma.currentPage.appendChild(node);
+  positionNodeToTheRight(node, null); // Pass null as the reference node
+}
+
+function appendToParentOrPage(node: SceneNode, referenceNode: SceneNode) {
+  const parent = referenceNode.parent && referenceNode.parent.type === 'FRAME' ? referenceNode.parent : figma.currentPage;
+  parent.appendChild(node);
+  positionNodeToTheRight(node, referenceNode);
+}
+
+function positionNodeToTheRight(node: SceneNode, referenceNode: SceneNode | null) {
+  if (referenceNode && 'x' in referenceNode) {
+    node.x = referenceNode.x + referenceNode.width + 10; // 10 is the gap between icons
+    node.y = referenceNode.y;
+  } else {
+    node.x = Math.round(figma.viewport.center.x);
+    node.y = Math.round(figma.viewport.center.y);
+  }
+  lastAppendedIcon = node;
+}
+
+figma.on('selectionchange', () => {
+  const currentSelection = figma.currentPage.selection;
+  if (lastAppendedIcon && !currentSelection.some(node => node === lastAppendedIcon)) {
+    lastAppendedIcon = null;
+  }
+});
